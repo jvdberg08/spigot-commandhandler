58d81b2c6eea4b56b3189631e429df6c60401ab5
Robert <robert061292@gmail.com>
2020-11-21 21:56:57 +0200

    Rework StringCommandArgument

--- src/main/java/com/github/jvdberg08/commandhandler/argument/StringCommandArgument.java
+++ src/main/java/com/github/jvdberg08/commandhandler/argument/StringCommandArgument.java
@@ -1,16 +1,20 @@
 package com.github.jvdberg08.commandhandler.argument;
 
+import org.apache.commons.lang.ArrayUtils;
+
+import java.util.Arrays;
+
 public class StringCommandArgument implements CommandArgument<String> {
 
-    private final String validArgument;
+    private final String[] validArguments;
 
-    public StringCommandArgument(String validArgument) {
-        this.validArgument = validArgument;
+    public StringCommandArgument(String... validArguments) {
+        this.validArguments = validArguments;
     }
 
     @Override
     public boolean checkArgument(String commandArgument, Object[] previousArguments) {
-        return commandArgument.equalsIgnoreCase(validArgument);
+        return ArrayUtils.isEmpty(validArguments) || Arrays.stream(validArguments).anyMatch(commandArgument::equalsIgnoreCase);
     }
 
     @Override
