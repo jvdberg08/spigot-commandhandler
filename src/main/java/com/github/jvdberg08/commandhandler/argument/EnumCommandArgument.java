6a90c0e25172f5128a7fa913a31edae58be2f355
Joren <jvdberg080403@gmail.com>
2020-11-22 19:52:01 +0100

    Empty StringCommandArguments will now be ignored, fixed potential error on EnumCommandArgument: Optional#get -> Optional#orElse(null)

--- src/main/java/com/github/jvdberg08/commandhandler/argument/EnumCommandArgument.java
+++ src/main/java/com/github/jvdberg08/commandhandler/argument/EnumCommandArgument.java
@@ -1,26 +1,24 @@
 package com.github.jvdberg08.commandhandler.argument;
 
+import org.apache.commons.lang.ArrayUtils;
+
 import java.util.Arrays;
-import java.util.stream.Collectors;
 
 public class EnumCommandArgument implements CommandArgument<Enum<?>> {
 
     private final Enum<?>[] validArgumentHolders;
 
     public EnumCommandArgument(Enum<?>... validArgumentHolders) {
-        if(validArgumentHolders == null) {
-            //TODO throw error
-        }
         this.validArgumentHolders = validArgumentHolders;
     }
 
     @Override
     public boolean checkArgument(String commandArgument, Object[] previousArguments) {
-        return Arrays.stream(validArgumentHolders).map(Enum::toString).collect(Collectors.toList()).contains(commandArgument);
+        return ArrayUtils.isEmpty(validArgumentHolders) || Arrays.stream(validArgumentHolders).map(Enum::toString).anyMatch(commandArgument::equalsIgnoreCase);
     }
 
     @Override
     public Enum<?> getArgument(String commandArgument, Object[] previousArguments) {
-        return Arrays.stream(validArgumentHolders).filter(enumVariable -> enumVariable.toString().equalsIgnoreCase(commandArgument)).findFirst().get();
+        return Arrays.stream(validArgumentHolders).filter(enumVariable -> enumVariable.toString().equalsIgnoreCase(commandArgument)).findFirst().orElse(null);
     }
 }
--- src/main/java/com/github/jvdberg08/commandhandler/argument/StringCommandArgument.java
+++ src/main/java/com/github/jvdberg08/commandhandler/argument/StringCommandArgument.java
@@ -2,26 +2,19 @@ package com.github.jvdberg08.commandhandler.argument;
 
 import org.apache.commons.lang.ArrayUtils;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class StringCommandArgument implements CommandArgument<String> {
 
     private final String[] validArguments;
 
     public StringCommandArgument(String... validArguments) {
-        if(validArguments == null) {
-            //TODO throw error
-        }
-        this.validArguments = Stream.of(validArguments).map(String::toLowerCase).toArray(String[]::new);
+        this.validArguments = validArguments;
     }
 
     @Override
     public boolean checkArgument(String commandArgument, Object[] previousArguments) {
-        return ArrayUtils.contains(validArguments, commandArgument.toLowerCase());
+        return ArrayUtils.isEmpty(validArguments) || Arrays.stream(validArguments).anyMatch(commandArgument::equalsIgnoreCase);
     }
 
     @Override
