d3bbeb449743116c78acce958a253cb76095973a
Joren van den Berg <jvdberg080403@gmail.com>
2021-02-12 22:40:24 +0100

    Added option for case sensitivity to built-in Enum and String CommandArguments

--- src/main/java/com/github/jvdberg08/commandhandler/argument/StringCommandArgument.java
+++ src/main/java/com/github/jvdberg08/commandhandler/argument/StringCommandArgument.java
@@ -5,13 +5,17 @@ import org.bukkit.command.CommandSender;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
 import java.util.stream.Collectors;
 
 public class StringCommandArgument implements CommandArgument<String> {
 
+    private final boolean caseSensitive;
     private final String[] validArguments;
 
-    public StringCommandArgument(String validArgument, String... validArguments) {
+    public StringCommandArgument(boolean caseSensitive, String validArgument, String... validArguments) {
+        this.caseSensitive = caseSensitive;
+
         List<String> list = new ArrayList<>();
         list.add(validArgument);
         list.addAll(Arrays.asList(validArguments));
@@ -20,7 +24,7 @@ public class StringCommandArgument implements CommandArgument<String> {
 
     @Override
     public boolean checkArgument(String commandArgument, List<Object> previousArguments) {
-        return Arrays.stream(validArguments).anyMatch(commandArgument::equalsIgnoreCase);
+        return Arrays.stream(validArguments).anyMatch(caseSensitive ? commandArgument::equals : commandArgument::equalsIgnoreCase);
     }
 
     @Override
@@ -30,7 +34,8 @@ public class StringCommandArgument implements CommandArgument<String> {
 
     @Override
     public List<String> tabComplete(CommandSender sender, String[] args) {
-        return Arrays.stream(validArguments).filter(str -> str.startsWith(args[args.length - 1])).collect(Collectors.toList());
+        return Arrays.stream(validArguments).filter(str -> caseSensitive
+                ? str.startsWith(args[args.length - 1])
+                : str.toLowerCase().startsWith((args[args.length - 1]).toLowerCase())).collect(Collectors.toList());
     }
-
 }
